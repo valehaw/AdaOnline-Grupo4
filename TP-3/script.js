e0714eef56a6fc516c522e86bcf75f8e194c7b75
ValerieVal <valehaw@hotmail.com>
2021-01-20 15:42:52 -0300

    creacion de la tabla de usuarios

--- TP-3/script.js
+++ TP-3/script.js
@@ -1,5 +1,19 @@
 const base = "https://5fd39ad7e9cda40016f5b79f.mockapi.io/";
 
+
+const table = document.createElement('table');
+const thead = document.createElement('thead');
+const tbody = document.createElement('tbody');
+
+
+table.setAttribute('border', '1');
+
+table.appendChild(thead);
+table.appendChild(tbody);
+document.body.appendChild(table);
+
+
+
 fetch(`${base}users`)
     .then(response => {
         return response.json();
@@ -8,7 +22,15 @@ fetch(`${base}users`)
     .then(data => {
 
         for (user of Object(data)){
-            console.log(data);          
+            const tr = document.createElement('tr');
+
+            for(prop in user){
+                const td = document.createElement('td');
+                td.innerHTML = user[prop];
+
+                tr.appendChild(td);
+            }
+            tbody.appendChild(tr);            
         }
 
     })
