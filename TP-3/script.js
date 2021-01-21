030f3449cf0ebcbed55865eb70dde0de1963cbd3
ValerieVal <valehaw@hotmail.com>
2021-01-21 16:27:37 -0300

    datos requeridos (nombre, mail, etc), footer, web-responsive, modal de btn add user

--- TP-3/script.js
+++ TP-3/script.js
@@ -1,52 +1,38 @@
 const base = "https://5fd39ad7e9cda40016f5b79f.mockapi.io/";
 
 
-const section = document.getElementById('tcontainer');
-
-const table = document.createElement('table');
-const thead = document.createElement('thead');
-const tbody = document.createElement('tbody');
-
-
-// table.setAttribute('border', '1');
-
-table.appendChild(thead);
-table.appendChild(tbody);
-section.appendChild(table);
-
-
-
 fetch(`${base}users`)
-    .then(response => {
-        return response.json();
+    .then(response => response.json()
+    )
+    .then(data => {
+        tableInfo(data)      
     })
 
-     .then(data => {   
-        const trHead = document.createElement('tr');
-        
-        for(prop in Object(data)[0]){
-            th = document.createElement('th');
-            th.innerHTML = prop;
-            trHead.appendChild(th);
-        }
-        thead.appendChild(trHead);
 
+const tableInfo = (users) => {
+    document.getElementById('tbody').innerHTML = "";
+    
+    for (let prop in users) {
+     
+        const user = users[prop];
+        const tr = document.createElement('tr');
+        const name = document.createElement('td');
+        const email = document.createElement('td');
+        const address = document.createElement('td');
+        const phone = document.createElement('td');
 
-        for (user of Object(data)){
-            const tr = document.createElement('tr');
+      
+        name.innerHTML = user.fullname;
+        email.innerHTML = user.email;
+        address.innerHTML = user.address;
+        phone.innerHTML = user.phone;
+    
+       
+        tr.appendChild(name);
+        tr.appendChild(email);
+        tr.appendChild(address);
+        tr.appendChild(phone);
 
-            for(prop in user){
-                const td = document.createElement('td');
-                td.innerHTML = user[prop];
-
-                tr.appendChild(td);
-            }
-            tbody.appendChild(tr);            
-        }
-
-    })
-
-    .catch(error => console.log("error en GET /users: " + error));
-
-
-
+        tbody.appendChild(tr);
+    }
+}
