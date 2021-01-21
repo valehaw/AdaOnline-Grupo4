776a219c9a509992422344fe731248380d589e4d
ValerieVal <valehaw@hotmail.com>
2021-01-21 20:44:53 -0300

    funcion delete hechagit add TP-3git add TP-3

--- TP-3/script.js
+++ TP-3/script.js
@@ -1,3 +1,6 @@
+const infoErased = document.getElementById('erase-info');
+
+
 const base = "https://5fd39ad7e9cda40016f5b79f.mockapi.io/";
 
 
@@ -11,6 +14,10 @@ fetch(`${base}users`)
 
 
 /************* Creación de tabla: INFO REQUERIDA Y BOTONES EDIT / DELETE **************/
+
+const table = document.getElementById('list');
+const tbody = document.querySelector('tbody');
+
 
 const tableInfo = (users) => {
     document.getElementById('tbody').innerHTML = "";
@@ -29,7 +36,7 @@ const tableInfo = (users) => {
         email.innerHTML = user.email;
         address.innerHTML = user.address;
         phone.innerHTML = user.phone;
-    
+        tr.setAttribute('id', user.id) //para poder borrar empleado
        
         tr.appendChild(name);
         tr.appendChild(email);
@@ -59,10 +66,40 @@ const actionsSection = () => {
         deleteBtn.setAttribute('data-toggle', 'modal');
         deleteBtn.setAttribute('data-target', '#deleteuser');
 
-
+    
+        deleteBtn.addEventListener("click", () => {
+            infoErased.value = row.id;
+        });
 
         tableData.appendChild(editBtn);
         tableData.appendChild(deleteBtn);
         row.appendChild(tableData);
     })
 }
+
+
+/************* FUNCION PARA BORRAR EMPLEADO **************/
+
+const delBtn = document.getElementById('del-user');
+
+const userErased = (id) => {
+    fetch(`${base}users/${id}`, {
+        method: 'DELETE'
+    })
+        .then(response => {
+            if (response) {
+                location.href = 'index.html';
+            }
+            return response.json()
+        })
+        
+};
+
+// evento del botón DELETE = YES, IM SURE
+delBtn.addEventListener('click', () => {
+    const infoErased = document.getElementById('erase-info');
+    userErased(infoErased.value);
+});
+
+
+
