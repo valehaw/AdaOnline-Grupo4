485bb4f012e49ba6c0fcca363d5b38459c7932d0
ValerieVal <valehaw@hotmail.com>
2021-01-25 20:38:54 -0300

    Clase 85 actualizada

---
+++ Clase-85-Practicas/script.js
@@ -0,0 +1,36 @@
+//Petición POST (Guardar datos)
+//JSON.Stringify
+//JSON.parse
+
+
+
+// const urlBase = "http://server.formacion-continua-web-api.development.magnetico.com.ar";
+
+// const user = {
+//     name: "Adrian",
+//     email: "adrian@ada.com",
+//     password: "asd123"
+// }
+
+// fetch(user + '/users')
+//     .then((response) => {
+
+//     });
+
+// fetch(url + '/users', {
+//     method: 'POST',
+//     headers: {
+//         "Content-Type: "Application/json",
+//     },
+//     body: tag,
+// });
+
+
+
+
+    // .then(pepe => { //nosotros ponemos el nombre, pero como estamos usando una API lo normal es usar
+    //                 //response
+    //     return pepe.json()
+    // }).then(data => {
+    //     console.log(data);
+    // });
