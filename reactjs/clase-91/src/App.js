8b226480421bf58ff2ab104e9f6c9d2a7b635c12
ValerieVal <valehaw@hotmail.com>
2021-02-17 23:54:20 -0300

    clase 91 terminada

---
+++ reactjs/clase-91/src/App.js
@@ -0,0 +1,14 @@
+import { Button } from './components'
+import React, { Fragment } from 'react'
+
+
+const App = () =>{
+  return (
+    <Fragment>
+      <h1>Valerie Waltz</h1>
+      <Button className= "btn btn-primary" />
+    </Fragment>
+  );
+}
+
+export default App;
---
+++ reactjs/clase-91/src/components/Button/index.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export const Button = () => {
+    return (
+        <button>
+            Mi Boton
+        </button>
+    )
+};
+
